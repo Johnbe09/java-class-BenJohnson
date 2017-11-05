08b759860cf9bff0cf75b238e5fb96f53f18b4de
Ben <johnbe09@luther.edu>
2017-11-05 12:50:54 -0600

    exercise4

--- src/exercise4/Ex4Driver.java
+++ src/exercise4/Ex4Driver.java
@@ -1,27 +1,79 @@
 package exercise4;
-
+import java.util.*;
 /**
  * @author yasiro01
  */
 public class Ex4Driver {
   /* Define a function that takes 1 integer as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task1(Integer);
+  public Integer task1(Integer n) {
+      // take an integer and return the integer + 1
+      return n + 1;
+  }
+    
   /* Define a function that takes 1 string as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task2(String);
+  public Integer task2(String s) {
+      //takes a string and returns the length of the string
+      int n = 0;
+      n = s.length();
+      return n;
+  }
   /* Define a function that takes 1 character as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task3(Character);
+  public Integer task3(Character c) {
+      // takes a character and returns the number 3
+      int n = 3;
+      return n;
+  }
+  
  /* Define a function that takes 1 string as a parameter and returns a character. Write 2 unit tests to verify the function validity. */
-//  public Character task4(String);
+  public Character task4(String s) {
+      // takes a string and returns the first character
+    Character c = null;
+    c = s.charAt(0);
+    return c;
+      
+  }
+    
   /* Define a function that takes 2 integers as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task5(Integer, Integer);
+  public Integer task5(Integer x, Integer y)  {
+      // takes two integers and returns the sum
+      int z = x + y;
+return z;
+        }
   /* Define a function that takes a string and an integer as parameters and returns a Character. Write 2 unit tests to verify the function validity. */
-//  public Character task6(String, Integer);
+  public Character task6(String x, Integer y) {
+     // takes a string and integer and returns character at the yth position
+      Character c = 'B';
+      c = x.charAt(y);
+      return c;
+  }
   /* Define a function that takes an array of integers as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task7(Integer[]);
+  public Integer task7(Integer[] a) {
+     // takes an array of integers and returns the first integer in the array
+      int i = 0;
+      i = a[0];
+      return i;
+  }
   /* Define a function that takes an array of integers and an integer as parameters and returns a boolean. Write 2 unit tests to verify the function validity. */
-//  public Boolean task8(Integer[], Integer);
+  public Boolean task8(Integer[] a, Integer i) {
+      // takes an array of integers and an integer and returns true if the integer is the same as the length of the array; false otherwise
+      boolean b = false;
+      int length = a.length;
+      if (i == length) {
+         b = true;
+      }
+      return b;
+  }
   /* Define a function that takes an ArrayList of Strings as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task9(ArrayList<String>);
+  public Integer task9(ArrayList<String> s) {
+      // takes a arraylist of strings and returns the size of the list
+      int i = 0;
+      i = s.size();
+      return i;
+  }
   /* Define a function that takes a HashMap (exact details are up to you) as a parameter and returns an integer. Write 2 unit tests to verify the function validity. */
-//  public Integer task10(HashMap<>);
+  public Integer task10(HashMap<String, Integer> h) {
+      // takes a hashmap as a parameter and returns 0
+      int i = 0;
+      return i;
+  }
 }
--- test/exercise4/Ex4DriverTest.java
+++ test/exercise4/Ex4DriverTest.java
@@ -5,6 +5,8 @@
  */
 package exercise4;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -36,5 +38,219 @@ public class Ex4DriverTest {
   @After
   public void tearDown() {
   }
+
+    /**
+     * Test of task1 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask1() {
+        // take integer and return one more than the integer
+        System.out.println("task1");
+        Integer n = 4;
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 5;
+        Integer result = instance.task1(n);
+        assertEquals(expResult, result);
+        n = -4;
+        expResult = -3;
+        result = instance.task1(n);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task2 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask2() {
+        // take string and return the length
+        System.out.println("task2");
+        String s = "task2";
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 5;
+        Integer result = instance.task2(s);
+        assertEquals(expResult, result);
+        s = ";;:";
+        expResult = 3;
+        result = instance.task2(s);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task3 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask3() {
+        // take character and return 3
+        System.out.println("task3");
+        Character c = 'C';
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 3;
+        Integer result = instance.task3(c);
+        assertEquals(expResult, result);
+        c = '!';
+        expResult = 3;
+        result = instance.task3(c);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task4 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask4() {
+        // takes a string and returns the first character
+        System.out.println("task4");
+        String s = "task4";
+        Ex4Driver instance = new Ex4Driver();
+        Character expResult = 't';
+        Character result = instance.task4(s);
+        assertEquals(expResult, result);
+        s = "CS252";
+        expResult = 'C';
+        result = instance.task4(s);
+        assertEquals(expResult, result);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task5 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask5() {
+         // takes two integers and returns the sum
+        System.out.println("task5");
+        Integer x = 4;
+        Integer y = 5;
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 9;
+        Integer result = instance.task5(x, y);
+        assertEquals(expResult, result);
+        x = 4;
+        y = -3;
+        expResult = 1;
+        result = instance.task5(x, y);
+        assertEquals(expResult, result);
+     
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task6 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask6() {
+             // takes a string and integer and returns character at the yth + 1 position
+
+        System.out.println("task6");
+        String x = "task6";
+        Integer y = 4;
+        Ex4Driver instance = new Ex4Driver();
+        Character expResult = '6';
+        Character result = instance.task6(x, y);
+        assertEquals(expResult, result);
+        x = "CS252";
+        y = 3;
+        expResult = '5';
+        result = instance.task6(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task7 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask7() {
+             // takes an array of integers and returns the first integer in the array
+
+        System.out.println("task7");
+        Integer[] a = new Integer[5];
+        a[0] = 4;
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 4;
+        Integer result = instance.task7(a);
+        assertEquals(expResult, result);
+        a[0] = 5;
+        expResult = 5;
+        result = instance.task7(a);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task8 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask8() {
+      // takes an array of integers and an integer and returns true if the integer is the same as the length of the array; false otherwise
+        System.out.println("task8");
+        Integer[] a = new Integer[5];
+        Integer i = 4;
+        Ex4Driver instance = new Ex4Driver();
+        Boolean expResult = false;
+        Boolean result = instance.task8(a, i);
+        assertEquals(expResult, result);
+        i = 5;
+        expResult = true;
+        result = instance.task8(a, i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task9 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask9() {
+      // takes a arraylist of strings and returns the size of the list
+        System.out.println("task9");
+        ArrayList<String> s = new ArrayList();
+        s.add("CS252");
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 1;
+        Integer result = instance.task9(s);
+        assertEquals(expResult, result);
+        s.add("CS165");
+        expResult = 2;
+        result = instance.task9(s);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of task10 method, of class Ex4Driver.
+     */
+    @Test
+    public void testTask10() {
+              // takes a hashmap as a parameter and returns 0
+
+        System.out.println("task10");
+        HashMap<String, Integer> h = new HashMap<>();
+        h.put("Dog", 1);
+        h.put("Cat", 2);
+        Ex4Driver instance = new Ex4Driver();
+        Integer expResult = 0;
+        Integer result = instance.task10(h);
+        assertEquals(expResult, result);
+        h.put("Elephant", 40);
+        expResult = 0;
+        result = instance.task10(h);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
 
 }
